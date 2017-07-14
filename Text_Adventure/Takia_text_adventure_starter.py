f71c9902c310e97f8e5f593c3a9c2035f8d4709a
Girls Who Code <girlswhocode@Girlss-MacBook-Pro.local>
2017-07-14 10:21:27 -0400

    adding text adventure file

---
+++ Text_Adventure/Takia_text_adventure_starter.py
@@ -0,0 +1,34 @@
+start = '''
+You wake up one morning and find that you aren't in your bed; you aren't even in your room.
+You're in the middle of a giant maze.
+A sign is hanging from the ivy: "You have one hour. Don't touch the walls."
+There is a hallway to your right and to your left.
+'''
+
+
+print(start)
+
+
+print("Type 'left' to go left or 'right' to go right.")
+user_input = input()
+if user_input == "left":
+    print("You decide to go left and... you find out that you are the Queen of a suffering over populated kingdom. Your people are suffering from hunger and they are ready to even sell their own childern to fill up their own stomach. You have to choice between meeting with one of the most powerful countries in order to beg for help for saving your kingdom or to meet the worlds most powerful warlock in order to use his magic to save your kingdom. ") # finished the story by writing what happens
+print("Type 'treaty' or 'warlock' to decide your future.")
+user_input= input()
+
+if user_input == warlock
+    print ("You have met the warlock and you have sold your soul in order to ensure that your kingdom is wellfeed and happy. However after selling your soul your kingdom was attacked by the kingdom you were going to have a treaty with!! Your people were all brutally murdered. You have lost everything.")
+else
+    print ("You have decided to make a treaty with Montugue, one of the most powerful countries in the world, however in exchange for food, money, and equipment; you must give away 500 of your people away every month. The king has demanded that 300 of those people be childern and if you refuse this treaty then you will be killed before you can say another word. You end up agreeing to the treaty, 3 years later your people are revolting against you. One night, a buch of rebels sneak into the kingdom and brutally murder you for the enslavement of their family memebers." )
+
+elif user_input == "right":
+    print("You choose to go right and ... you are a pirate. Every single kingdom has felt the wrath of your greed. Every other pirate wishes to be as powerul as you and you have an endless amount of enemies. One day, you and your crew stumbles upon a empty ship coverd in gold. Do you loot it or do you ignore it?  ") # finished the story writing what happens
+ print("Type 'loot' or 'ignore' to decide your future.")
+ user_input= input()
+
+ if user_input ==loot
+     print("you have entered the ship. Your men are upstairs collecting the goods on top of the ship whereas you are inspecting the bottom levels of the ship when all of a sudden you are stabbed in the back of your right hand man. You are left to die while your crew rows away to the shore")
+else
+     print (" you have chosen to ignore the boat. All of a sudden a blast is heard. Your ship was attacked. It turns out that the empty boat was not so empty, the boat belonged to one of your enemies who seeked revenged.Their canons have destroyed your ship and your ship is sinking, along with all of your collected gold. You had two choices, either save yourself or go after a lifetime of gold and try to save some.Sadly, you die in ocean due to the lack of oxygen.")
+end of story=
+    print ("You wake up from your dream. When you look around your room, you notice a mural of a kingdom and a ship surronding the walls of your bedroom. You have no idea how the mural was painted in your room, you choose to ignore it and you walk out of your bedroom, not realising that the ship was moving.  ")
